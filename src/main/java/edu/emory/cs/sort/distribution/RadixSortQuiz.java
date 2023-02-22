e8d9b18f7c1749efee6e59f08ded3695a4fc9b46
WenyeSong <wenye.song@emory.edu>
2023-02-22 15:03:45 -0500

    git add --all

--- src/main/java/edu/emory/cs/sort/distribution/RadixSortQuiz.java
+++ src/main/java/edu/emory/cs/sort/distribution/RadixSortQuiz.java
@@ -6,38 +6,38 @@ import java.util.Deque;
 public class RadixSortQuiz extends RadixSort {
     @Override
     public void sort(Integer[] array, int beginIndex, int endIndex) {
-        // TODO: to be filled
         int maxBit = getMaxBit(array, beginIndex, endIndex);// find the max bit
         mySort(array, beginIndex, endIndex, maxBit);
     }
 
-    public void mySort(Integer[] array, int beginIndex, int endIndex, int bit) {
-        if (endIndex <= beginIndex || bit <= 0) {
+    public void mySort(Integer[] array, int indexNotNeedToSort, int endIndex, int bit) {
+        if (endIndex <= indexNotNeedToSort || bit <= 0) {
             return;
         }
-        int[] eachBucketNum = new int[buckets.size()];
+        int[] eachBucketNum = new int[buckets.size()]; // buckets.size() = 10
 
-        for (int i = beginIndex; i < endIndex; i++) {
+        for (int i = indexNotNeedToSort; i < endIndex; i++) {
             int digit = (array[i] / (int) Math.pow(10, bit - 1)) % 10; // digit on each bit
             eachBucketNum[digit]++; // how many number in this bucket of digit
         }
 
-        sort(array, beginIndex, endIndex, key -> (key / (int) Math.pow(10, bit - 1)) % 10); // bucket sort
+        sort(array, indexNotNeedToSort, endIndex, key -> (key / (int) Math.pow(10, bit - 1)) % 10); // bucket sort
 
-        int bucketNotNeedToSort = 0;
         for (int digit = 0; digit < buckets.size(); digit++) {
 
             if (eachBucketNum[digit] == 1)
-                bucketNotNeedToSort++;
+                indexNotNeedToSort++;
             else if(eachBucketNum[digit] != 1 && eachBucketNum[digit] != 0){
-                mySort(array, bucketNotNeedToSort, eachBucketNum[digit], bit - 1);
+                mySort(array, indexNotNeedToSort, eachBucketNum[digit] + indexNotNeedToSort, bit - 1);
+                indexNotNeedToSort += eachBucketNum[digit];
+
             }
         }
     }
 
- /*       public static void main (String[]args)
+   /*     public static void main (String[]args)
         {
-            Integer arr[] = {802, 883, 789, 14, 66, 2, 66, 100, 543, 289, 1000, 345, 145690}; //move out numbers from the bottom
+            Integer arr[] = {113,145,367,79866,66,23,66,8}; //move out numbers from the bottom
             int n = arr.length;
             RadixSortQuiz test = new RadixSortQuiz();
             test.sort(arr, 0, arr.length);
