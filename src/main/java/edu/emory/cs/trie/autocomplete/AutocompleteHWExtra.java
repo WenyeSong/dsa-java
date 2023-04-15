8331ad54658a2ebf67018eedf23f15b16c47c463
WenyeSong <wenye.song@emory.edu>
2023-04-15 10:18:36 -0400

    git add --all

--- src/main/java/edu/emory/cs/trie/autocomplete/AutocompleteHWExtra.java
+++ src/main/java/edu/emory/cs/trie/autocomplete/AutocompleteHWExtra.java
@@ -2,114 +2,145 @@ package edu.emory.cs.trie.autocomplete;
 import edu.emory.cs.trie.TrieNode;
 
 import java.util.*;
-import java.util.List;
-import java.util.Map;
 
-/**
- * @author Jinho D. Choi ({@code jinho.choi@emory.edu})
- */
 public class AutocompleteHWExtra extends Autocomplete<List<AutocompleteHWExtra.Reply>> {
+
+    public AutocompleteHWExtra(String dict_file, int max) {
+        super(dict_file, max);
+    }
 
     @Override
     public List<String> getCandidates(String prefix) {
-        // TODO: to be updated
-        TrieNode<List<Reply>> root = this.getRoot();
-        root = findLastLetter(root, prefix);
-
-        List<Reply> values = root.getValue();
-        List<String> ans = new ArrayList<>();
-        Collections.sort(values, new StringCompartor());
-
-        for (Reply item : values) {
-            String temp = prefix.substring(0, prefix.length() - 1) + item.word;
-            ans.add(temp);
+        if (prefix == null || prefix.trim().isEmpty()) {
+            return getInitialLetters();
+        } else {
+            prefix = prefix.trim();
         }
-        return ans.subList(0,getMax());
+        TrieNode<List<Reply>> nodeOfPrefix = find(prefix);
+
+        PriorityQueue<Reply> queue = new PriorityQueue<>(new StringComparator());
+
+        if (nodeOfPrefix != null) {
+            dfs(nodeOfPrefix, prefix, queue);
+        }
+
+        List<String> candidates = new ArrayList<>();
+        List<Reply> valueOfNode;
+
+        if (nodeOfPrefix != null) {
+            valueOfNode = nodeOfPrefix.getValue();
+            if (valueOfNode != null) {
+                for (Reply reply : valueOfNode) {
+                    candidates.add(reply.word);
+                }
+            }
+        }
+
+        while (!queue.isEmpty() && candidates.size() < getMax()) {
+            String candidate = queue.poll().word;
+            if (!candidates.contains(candidate)) {
+                candidates.add(candidate);
+            }
+        }
+        return candidates;
+    }
+
+    private List<String> getInitialLetters() {
+        Set<String> initialLetters = new HashSet<>();
+        for (TrieNode<List<Reply>> child : getRoot().getChildrenMap().values()) {
+            initialLetters.add(String.valueOf(child.getKey()));
+            if (initialLetters.size() == getMax()) {
+                break;
+            }
+        }
+        List<String> initialLettersList = new ArrayList<>(initialLetters);
+        Collections.sort(initialLettersList);
+        return initialLettersList;
+    }
+
+    private void dfs(TrieNode<List<Reply>> node, String prefix, PriorityQueue<Reply> queue) {
+        if (node.isEndState()) {
+            queue.add(new Reply(0, prefix)); // replace 0 with actual frequency if needed
+        }
+        for (Character key : node.getChildrenMap().keySet()) {
+            TrieNode<List<Reply>> child = node.getChild(key);
+            dfs(child, prefix + key, queue);
+        }
     }
 
     @Override
     public void pickCandidate(String prefix, String candidate) {
-        // TODO: to be updated
-        TrieNode<List<Reply>> root = this.getRoot();
-        root = findLastLetter(root, prefix);
-        List<Reply> values = root.getValue();
-        String temp = candidate.substring(1);
-        for (Reply r : values) {
-            if (r.word.equals(temp)) {
-                r.freq++;
+        prefix = prefix.trim().toLowerCase();
+        TrieNode<List<Reply>> nodeOfPrefix = find(prefix);
+        if (candidate == null) {
+            return;
+        }
+
+        if (nodeOfPrefix == null) {
+            put(prefix, new ArrayList<>());
+            nodeOfPrefix = find(prefix);
+            nodeOfPrefix.setEndState(false);
+        }
+        List<Reply> valueOfPrefix = nodeOfPrefix.getValue();
+        if (valueOfPrefix == null) {
+            valueOfPrefix = new ArrayList<>();
+            nodeOfPrefix.setValue(valueOfPrefix);
+        }
+
+        Reply candidateReply = null;
+        for (Reply reply : valueOfPrefix) {
+            if (reply.word.equals(candidate)) {
+                candidateReply = reply;
+                break;
             }
         }
-    }
-    public AutocompleteHWExtra(String dict_file, int max) {
-        super(dict_file, max);
-        generateList(this.getRoot());
-    }
-    public TrieNode<List<Reply>> findLastLetter(TrieNode<List<Reply>> root, String prefix) {
-        Map<Character, TrieNode<List<Reply>>> childrenMap;
-        for (int i = 0; i < prefix.length(); i++) {
-            childrenMap = root.getChildrenMap();
-            root = childrenMap.get(prefix.charAt(i));
+
+        if (candidateReply != null) {
+            candidateReply.updateTimestamp();
+            candidateReply.freq++;
+        } else {
+            candidateReply = new Reply(1, candidate);
+            candidateReply.updateTimestamp();
+            valueOfPrefix.add(candidateReply);
         }
-        return root;
-    }
-    public void generateList(TrieNode<List<Reply>> root) {
-        Map<Character, TrieNode<List<Reply>>> childrenMap = root.getChildrenMap();
-        List<Reply> valueList = new ArrayList<>();
-        for (Character key : childrenMap.keySet()) {
-            TrieNode<List<Reply>> child = root.getChild(key);
-            generateList(child);
-            for (Reply s : child.getValue()) {
-                valueList.add(new Reply(0, root.getKey() + s.word));
-            }
-        }
-        if (childrenMap.isEmpty() || root.isEndState()){
-            valueList.add(new Reply(0, String.valueOf(root.getKey())));
-        }
-        Collections.sort(valueList, new StringCompartor());
-        root.setValue(valueList);
+
+        nodeOfPrefix = find(candidate);
+        List<Reply> Value = nodeOfPrefix == null ? null : nodeOfPrefix.getValue();
+        put(candidate, Value);
     }
 
-    private static class StringCompartor implements Comparator<Reply> {
-        public int compare(Reply o1, Reply o2) {
+    private static class StringComparator implements Comparator<AutocompleteHWExtra.Reply> {
+        public int compare(AutocompleteHWExtra.Reply o1, AutocompleteHWExtra.Reply o2) {
             if (o1.freq != o2.freq) {
                 return o2.freq - o1.freq;
             } else {
-                if (o1.word.length() != o2.word.length()) {
-                    return o1.word.length() - o2.word.length();
-                }  else {
-                    return o1.word.compareTo(o2.word);
+                if (o1.timestamp != o2.timestamp) {
+                    return (int) (o2.timestamp - o1.timestamp);
+                } else {
+                    if (o1.word.length() != o2.word.length()) {
+                        return o1.word.length() - o2.word.length();
+                    } else {
+                        return o1.word.compareTo(o2.word);
+                    }
                 }
             }
         }
     }
 
-    public class Reply{
+    public class Reply {
         int freq;
         String word;
+        long timestamp;
+
         Reply(int freq, String word) {
             this.freq = freq;
             this.word = word;
+            this.timestamp = System.currentTimeMillis();
+        }
+
+        public void updateTimestamp() {
+            this.timestamp = System.currentTimeMillis();
         }
     }
-/*    public static void main(String[] args) {
-        final String dict_file = "src/main/resources/dict.txt";
-        final int max = 15;
-
-        Autocomplete<?> ac = new AutocompleteHWExtra(dict_file, max);
-
-        System.out.println(ac.getCandidates("ph"));
-        ac.pickCandidate("ph", "pho");
-        ac.pickCandidate("ph", "pho");
-        ac.pickCandidate("ph", "pho");
-        ac.pickCandidate("ph", "pho");
-        ac.pickCandidate("ph", "pho");
-        System.out.println(ac.getCandidates("ph"));
-
-        ac.pickCandidate("ph", "phi");
-        ac.pickCandidate("ph", "phi");
-        System.out.println(ac.getCandidates("ph"));
-
-        ac.pickCandidate("ph", "phr");
-        System.out.println(ac.getCandidates("ph"));
-    }*/
 }
+
